8d9963429a99cb54690f1ddbca5934e1132cf07b
Natalia Strelnikova <nata-n9@ya.ru>
2024-02-29 15:56:18 +0300

    rep

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array = { "poi", "teyurjf", "kjl",  "852", "52", "hello", "world", "56f"};
+string[] newArray = new string[array.Length];
+int newIndex = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <= 3)
+{newArray[newIndex] = array[i];
+newIndex++;
+}
+}
+
+for (int i = 0; i < newIndex; i++)
+{
+Console.Write($"{newArray[i]} ");
+}
+Console.WriteLine();
